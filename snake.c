4ea073d44118a30a54ae1a29fb6acfe9e46e2524
root <root@host.localdomain>
2018-07-28 19:02:00 +0800

    error windows.h and conin.h

--- snake.c
+++ snake.c
@@ -1,6 +1,27 @@
 /***************
  *fucktion:  snack.c
- *description: user c to write a clsaa game,snack.
+ *description: use c to write a clsaa game,snack.
  *name :wenzong
  *date: 20180728
  * *************/
+
+/*************************
+ *思路：
+ *1：数据设计
+ *2：模块化设计
+ *3：实现过程
+ * ************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+//#include <windows.h>	//光标设置的API
+#include <time.h>	//食物随机
+//#include <conio.h>	//按键监控
+
+int main()
+{
+
+    printf("GameOver");
+//    system("pause");	//防止闪停
+    return 0;
+}
